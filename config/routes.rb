01b6cb4d02b6dab7946b5e45fc74e92357799184
oobayashikaito <ooba.oneok7@icloud.com>
2020-01-20 16:53:56 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  get 'messages/index'
-
->>>>>>> chat-space-練習
   root "messages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
