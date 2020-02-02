218a82da70b14abe7f1e044596d1a9c71a966bc4
oobayashikaito <ooba.oneok7@icloud.com>
2020-02-02 18:42:34 +0900

    自動更新機能　up

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,31 +59,31 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    var reloadMessages = function() {
-      last_message_id = $('.message:last').data("message-id");
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-      })
-      .fail(function() {
-        console.log('error');
-      });
-    };
-  });
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
 });
-    
+
+var reloadMessages = function() {
+  last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+  var insertHTML = '';
+  $.each(messages, function(i, message) {
+    insertHTML += buildHTML(message)
+  });
+  $('.messages').append(insertHTML);
+  $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+  }
+  })
+  .fail(function() {
+    console.log('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+setInterval(reloadMessages, 7000);
+}
+});
